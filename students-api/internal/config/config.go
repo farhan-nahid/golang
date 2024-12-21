b8e4058a8532ab73db1a3ac4a448e53cbd05ac25
Farhan <nahidahmed9933@gmail.com>
2024-12-21 23:46:45 +0600

    feat: Init

---
+++ students-api/internal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Address string
+}
+
+type Config struct {
+	Env 		 string `yaml:"env" env:"ENV" env-required:"true" env-default:"production"`
+	Storage_path string `yaml:"storage_path" env:"STORAGE_PATH" env-required:"true"`
+	HTTPServer          `yaml:"http_server"`
+}
+
+
+func MustLoadConfig() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		flags := flag.String("config", "", "path to config file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("config path is required")
+		}
+	}
+
+	if _, err:= os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file not found: %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err.Error())
+	}
+
+	return &cfg
+}
